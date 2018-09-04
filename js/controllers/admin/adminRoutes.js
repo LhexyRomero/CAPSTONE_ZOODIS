ef5bfcfc3bb08502dda41c2b9837a21014298408
lhexyromero <lhexyromero@gmail.com>
2018-09-04 16:07:49 +0800

    Second Commit

---
+++ js/controllers/admin/adminRoutes.js
@@ -0,0 +1,193 @@
+
+const express = require('express');
+const router = express.Router();
+
+const upload = require('../../fileUpload');
+
+const auth = require('../authentication');
+const animal = require('../admin/animal');
+const bacteria = require('../admin/bacteria');
+const search = require('../admin/search');
+const disease = require('../admin/disease');
+const prevention = require('../admin/prevention');
+const notification = require('../admin/notification');
+const journal = require('../admin/journal');
+const reject = require('../admin/reject');
+const staff = require('../admin/staff');
+const sample = require('../admin/sample');
+
+router.get('/', (_,res,__)=>{
+    res.redirect('/dashboard');
+});
+
+router.route('/login')
+    .post(auth.login, auth.authRedirect)
+    .get(auth.authRedirect,(req,res,next)=>{
+        res.locals.logFail = req.query.failed || false;
+        res.render('login', res.locals);
+    });
+
+router.get('/logout', auth.logout); 
+
+router.get('/register',(req,res,next)=>{
+    var result = req.query.error || false;
+    res.render('register',{regError: result});
+});
+router.post('/register', auth.register);
+
+//Search Router, for searching taxonomy of animal and bacteria
+router.get('/search/animal', search.animal);
+router.get('/search/bacteriaGenus',search.bacteriaGenus);
+router.get('/search/bacteriaSpecies',search.bacteriaSpecies);
+
+router.get('/dashboard', auth.authenticate,(req,res,next)=>{
+    res.render('admin/dashboard');
+});
+
+router.get('/sampleAlgo',auth.authenticate,(req,res,next)=>{
+    res.render('admin/sampleAlgo');
+});
+
+router.post('/sample',sample.sample);
+
+router.get('/user',auth.authenticate,(req,res,next)=>{
+    res.render('admin/user');
+});
+
+router.get('/hostTable', auth.authenticate,(req,res,next)=>{
+    res.render('admin/hostTable');
+});
+
+router.get('/diseaseTable', auth.authenticate,(req,res,next)=>{
+    res.render('admin/diseaseTable');
+});
+
+router.get('/rejectTable', auth.authenticate,(req,res,next)=>{
+    res.render('admin/rejectTable');
+});
+
+router.get('/rejectTableList',auth.authenticate,reject.rejectTable);
+
+router.get('/animalTaxon', auth.authenticate, (req,res,next)=>{
+    res.render('admin/animalTaxon');
+});
+router.post('/animalTaxon', animal.addAnimalTaxon);
+router.post('/updateAnimalTaxon/:id',animal.updateAnimalTaxon);
+router.get('/animalTaxonList',animal.animalTaxonList);
+router.get('/editAnimalTaxon/:id',animal.editAnimalTaxon);
+
+router.get('/bacteriaTaxon', auth.authenticate, (req,res,next)=>{
+    res.render('admin/bacteriaTaxon');
+});
+router.post('/bacteriaTaxon',bacteria.addBacteriaTaxon);
+router.post('/updateBacteriaTaxon/:id',bacteria.updateBacteriaTaxon);
+router.get('/bacteriaTaxonList',bacteria.bacteriaTaxonList);
+router.get('/editBacteriaTaxon/:id',bacteria.editBacteriaTaxon);
+
+router.get('/animal', auth.authenticate, (req,res,next) =>{
+    res.render('admin/animal');
+});
+router.post('/animal', upload.single("animalImg"), animal.addAnimal);
+router.post('/editAnimal/:id', upload.single('animalImg'), animal.updateAnimal);
+router.get('/animalList',animal.animalList);
+router.get('/viewAnimal/:id',animal.viewAnimal);
+router.get('/toSelectJournal',animal.toSelectJournal);
+
+router.get('/bacteria', auth.authenticate, (req,res,next)=>{
+    res.render('admin/bacteria');
+});
+
+router.post('/bacteria',bacteria.addBacteria);
+router.post('/updateBacteria/:id',bacteria.updateBacteria);
+router.get('/bacteriaList',bacteria.bacteriaList);
+router.get('/toSelectBacteria',bacteria.toSelectBacteria);
+router.get('/toSelectBacteria2',bacteria.toSelectBacteria2);
+router.get('/toSelectJournalBacteria',bacteria.toSelectJournalBacteria);
+router.get('/toModalSelect',bacteria.toSelectBacteria2);
+router.get('/viewBacteria/:id',bacteria.viewBacteria);
+router.get('/editBacteria/:id',bacteria.viewBacteria);
+
+router.get('/disease', auth.authenticate,(req,res,next)=>{
+    res.render('admin/disease');
+});
+router.post('/disease',disease.addDisease);
+router.get('/diseaseList',disease.diseaseList);
+router.get('/toSelectBacteriaDisease',disease.toSelectBacteriaDisease);
+router.get('/viewDisease/:id',disease.viewDisease);
+router.get('/toSelectJournalDisease2',disease.toSelectJournalDisease);
+router.post('/editDisease/:id',disease.editDisease);
+
+
+router.get('/toxin', auth.authenticate, (req,res,next)=>{
+    res.render('admin/toxin');
+});
+router.post('/toxin',bacteria.addToxin);
+router.get('/toxinList',bacteria.toxinList);
+router.get('/editToxin/:id',bacteria.editToxin);
+router.post('/updateToxin/:id',bacteria.updateToxin);
+
+router.get('/prevention', auth.authenticate, (req,res,next)=>{
+    res.render('admin/prevention');
+});
+router.get('/toSelectDisease',prevention.toSelectDisease);
+router.get('/preventionList',prevention.preventionList);
+router.get('/viewPrevention/:id',prevention.viewPrevention);
+router.get('/editPrevention/:id',prevention.viewPrevention);
+router.post('/updatePrevention/:id',prevention.updatePrevention);
+router.post('/addPrevention',prevention.addPrevention);
+
+router.get('/notification',auth.authenticate,(req,res,next) =>{
+    res.render('admin/notification');
+});
+
+router.get('/notificationList',notification.notificationList);
+router.get('/notificationViewAnimalTaxo/:id',notification.viewAnimalTaxo);
+router.get('/notificationViewBacteriaTaxo/:id',notification.viewBacteriaTaxo);
+router.get('/notificationSelectBacteria',notification.selectBacteria);
+router.get('/notificationViewToxin/:id',notification.viewToxin);
+router.get('/notificationViewDisease/:id',notification.viewDisease);
+router.get('/notificationSelectDisease',notification.selectDisease);
+router.get('/notificationViewPrevention/:id',notification.viewPrevention);
+router.get('/notificationViewAnimal/:id',notification.viewAnimal);
+router.get('/notificationSelectAnimal',notification.selectAnimal);
+router.get('/notificationViewBacteria/:id',notification.viewBacteria);
+
+router.post('/approvedAnimalTaxo/:id',notification.approvedAnimalTaxo);
+router.post('/rejectAnimalTaxo/:id',notification.rejectAnimalTaxo);
+router.post('/approvedBacteriaTaxo/:id',notification.approvedBacteriaTaxo);
+router.post('/rejectBacteriaTaxo/:id',notification.rejectBacteriaTaxo);
+router.post('/approvedToxin/:id',notification.approvedToxin);
+router.post('/rejectToxin/:id',notification.rejectToxin);
+router.post('/notificationApprovedDisease/:id',notification.approvedDisease);
+router.post('/rejectDisease/:id',notification.rejectDisease);
+router.post('/notificationApprovedPrevention/:id',notification.approvedPrevention);
+router.post('/rejectPrevention/:id',notification.rejectPrevention);
+router.post('/approvedBacteria/:id',notification.approvedBacteria);
+router.post('/rejectBacteria/:id',notification.rejectBacteria);
+router.post('/approvedAnimal/:id',notification.approvedAnimal);
+router.post('/rejectAnimal/:id',notification.rejectAnimal);
+
+router.get('/journal',auth.authenticate,(req,res,next)=>{
+    res.render('admin/journal');
+});
+
+router.get('/toSelectStaffName',journal.toSelectStaffName);
+router.get('/toSelectJournal1',journal.toSelectJournal);
+router.get('/journalList',journal.journalList);
+router.get('/editJournal/:id',journal.editJournal);
+router.get('/viewJournal/:id',journal.viewJournal);
+
+router.post('/addJournal',upload.single('myfile'),journal.addJournal);
+router.post('/updateJournal/:id',journal.updateJournal);
+router.post('/assignedJournal',journal.assignedJournal);
+
+router.get('/staffTable',auth.authenticate, (req,res,next)=>{
+    res.render('admin/staff');
+});
+
+router.get('/staffList',staff.staffList);
+router.post('/code',staff.code);
+router.post('/updateTypeToAdmin/:id',staff.updateTypeToAdmin);
+router.post('/updateTypeToContributor/:id',staff.updateTypeToContributor);
+
+module.exports = router;
---
+++ js/controllers/contributors/contriRoutes.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const router = express.Router();
+
+const upload = require('../../fileUpload');
+
+const auth = require('../authentication');
+const contri_auth = require('../contributors/auth');
+const contri_animal = require('../contributors/animal');
+const contri_bacteria = require('../contributors/bacteria');
+const contri_disease = require('../contributors/disease');
+const contri_prevention = require('../contributors/prevention');
+const contri_notification = require('../contributors/notification');
+
+
+router.get('/contri_*', auth.authenticate, (req,res,next)=>{
+    if(req.session.accType == 1) return next();
+    res.redirect('/login');
+}); 
+
+router.get('/contri_Animal',auth.authenticate,(req,res,next)=>{
+    res.render('contributor/contri_Animal');
+});
+
+router.post('/contri_animal',upload.single("contri_animalImg"), contri_auth, contri_animal.addAnimal);
+
+router.get('/contri_viewAnimal/:id',contri_animal.viewAnimal);
+router.get('/contri_animalList',contri_animal.animalList);
+
+router.get('/contri_animalTaxon',auth.authenticate,(req,res,next)=>{
+    res.render('contributor/contri_AnimalTaxon');
+});
+
+router.post('/contri_animalTaxon', contri_auth, contri_animal.addAnimalTaxon);
+
+router.get('/contri_animalTaxonList',contri_animal.animalTaxonList);
+router.get('/contri_viewAnimalTaxon/:id',contri_animal.viewAnimalTaxon);
+router.get('/contri_toSelectJournal',contri_animal.toSelectJournal);
+
+router.get('/contri_Bacteria',auth.authenticate,(req,res,next)=>{
+    res.render('contributor/contri_Bacteria');
+});
+
+router.post('/contri_bacteriaTaxon', contri_auth, contri_bacteria.addBacteriaTaxon);
+router.post('/contri_bacteria', contri_auth, contri_bacteria.addBacteria);
+
+router.get('/contri_bacteriaList',contri_bacteria.bacteriaList);
+router.get('/contri_bacteriaTaxonList',contri_bacteria.bacteriaTaxonList);
+router.get('/contri_viewBacteriaTaxon/:id',contri_bacteria.viewBacteriaTaxon);
+router.get('/contri_toSelectJournal1',contri_bacteria.toSelectJournal);
+router.get('/contri_toSelectAnimal',contri_bacteria.toSelectAnimal);
+router.get('/contri_viewBacteria/:id',contri_bacteria.viewBacteria);
+
+router.get('/contri_BacteriaTaxon',auth.authenticate,(req,res,next)=>{
+    res.render('contributor/contri_BacteriaTaxon');
+});
+
+router.get('/contri_Disease',auth.authenticate,(req,res,next)=>{
+    res.render('contributor/contri_Disease');
+});
+
+router.post('/contri_disease', contri_auth, contri_disease.addDisease);
+router.get('/contri_toSelectBacteriaDisease',contri_disease.toSelectBacteriaDisease);
+router.get('/contri_toSelectJournalDisease',contri_disease.toSelectJournalDisease);
+router.get('/contri_diseaseList',contri_disease.diseaseList);
+router.get('/contri_viewDisease/:id',contri_disease.viewDisease);
+
+router.get('/contri_Prevention',auth.authenticate,(req,res,next)=>{
+    res.render('contributor/contri_Prevention');
+});
+
+router.post('/contri_addPrevention', contri_auth, contri_prevention.addPrevention);
+router.get('/contri_toSelectDisease',contri_prevention.toSelectDisease);
+router.get('/contri_preventionList',contri_prevention.preventionList);
+
+router.get('/contri_Toxin',auth.authenticate,(req,res,next)=>{
+    res.render('contributor/contri_Toxin');
+});
+
+router.post('/contri_toxin', contri_auth, contri_bacteria.addToxin);
+
+router.get('/contri_toxinList',contri_bacteria.toxinList);
+router.get('/contri_toSelectBacteria' , contri_bacteria.toSelectBacteria);
+router.get('/contri_viewToxin/:id',contri_bacteria.viewToxin);
+
+router.get('/contri_Dashboard', auth.authenticate, (req,res,next)=>{
+    res.render('contributor/contri_Dashboard');
+});
+
+router.get('/contri_Notification', auth.authenticate, (req,res,next)=>{
+    res.render('contributor/contri_Notification');
+});
+
+router.get('/notiCard',contri_notification.notiCard);
+router.get('/notifyJournal',contri_notification.notifyJournal);
+router.get('/downloadJournal/:filename',contri_notification.downloadJournal);
+
+router.post('/setJournal',contri_notification.setJournal);
+router.post('/updateNotiCard/:id',contri_notification.updateNotiCard);
+router.post('/finishedJournal',contri_notification.finishedJournal);
+
+
+module.exports = router;
