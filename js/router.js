c239fd34653f43348bb1e97226f53742d6e3300f
Paulymorphy <christianpaultupas@gmail.com>
2018-09-20 18:27:54 +0800

    Working Get Species API

--- js/router.js
+++ js/router.js
@@ -5,15 +5,6 @@ router.use('/',require("./controllers/admin/adminRoutes"));
 router.use('/',require("./controllers/contributors/contriRoutes"));
 router.use('/',require("./controllers/researcher/researcherRoutes"));
 
-router.get('/test', (req, res, next)=>{
-    let bacdive = require('./bacdive');
-    if(!req.query.genus) return res.status(404).end();
-    bacdive.searchSpecies(req.query.genus, function(err, data){
-        if(err) return next(err);
-        res.send(data);
-    })
-});
-
 router.use((req,res)=>{
     let response;
     if(req.xhr){
@@ -25,7 +16,7 @@ router.use((req,res)=>{
 });
 
 router.use((err,req,res,next)=>{
-    console.error(err);
+    console.error(err.message, err.stack);
     let response;
     if(req.xhr){
         response = {success: false};
