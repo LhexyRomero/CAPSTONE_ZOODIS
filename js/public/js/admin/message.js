e0718e60f0be8c65b686a26eb25e6b71f3bcf42d
lhexyromero <lhexyromero@gmail.com>
2018-10-02 15:10:53 +0800

    Updated Database

--- js/public/js/admin/message.js
+++ js/public/js/admin/message.js
@@ -57,7 +57,7 @@ function messageList(limit, offset) {
                 row += "<td><br><div class='form-check'><label class'form-check-label'><input class='form-check-input' type='checkbox'><span class='form-check-sign'></span></label></div></td>"
                 row += "<td><strong>" + element.name + "</strong></td>";
                 row += "<td><strong>" + element.subject + "</strong></td>";
-                row += "<td><label>" + Date.parse(element.date).toString('MMM dd') + "</label></td>";
+                //row += "<td><label>" + Date.parse(element.dateTime).toString('MMM dd') + "</label></td>";
                 row += "</tr>";
                 html += row;
             }
@@ -66,7 +66,7 @@ function messageList(limit, offset) {
                 row += "<td><br><div class='form-check'><label class'form-check-label'><input class='form-check-input' type='checkbox'><span class='form-check-sign'></span></label></div></td>"
                 row += "<td>" + element.name + "</td>";
                 row += "<td>" + element.subject + "</td>";
-                row += "<td><label>" + Date.parse(element.date).toString('MMM dd') + "</label></td>";
+               // row += "<td><label>" + Date.parse(element.date).toString('MMM dd') + "</label></td>";
                 row += "</tr>";
                 html += row;
             }
